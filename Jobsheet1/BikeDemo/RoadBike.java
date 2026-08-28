0af5b696586ed74f6543a4962712a06d6058ff99
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2026-08-28 09:23:34 +0700

    Percobaan 2

---
+++ Jobsheet1/BikeDemo/RoadBike.java
@@ -0,0 +1,17 @@
+package Jobsheet1.BikeDemo;
+
+public class RoadBike extends Bike {
+
+    private int tireWidth;
+
+    public void setTireWidth(int width){
+        tireWidth = width;
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Tire Width : " + tireWidth + " mm");
+        System.out.println("Bike Type  : Road Bike");
+    }
+}
