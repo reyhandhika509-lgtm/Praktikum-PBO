c46eb36346f07aaaf2f8ea9e18636affda5e2912
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2026-09-02 11:34:34 +0700

    Image

--- Jobsheet1/BikeDemo/BikeDemo.java
+++ Jobsheet1/BikeDemo/BikeDemo.java
@@ -4,6 +4,7 @@ public class BikeDemo {
     public static void main(String[] args) {
         Bike mountainBike1 = new Bike();
         Bike mountainBike2 = new Bike();
+        RoadBike roadBike1 = new RoadBike(); 
 
         mountainBike1.setBrand("Trek");
         mountainBike1.speedAcceleration(10);
@@ -14,6 +15,12 @@ public class BikeDemo {
         mountainBike2.speedAcceleration(20);
         mountainBike2.gearChanges(3);
         mountainBike2.printInfo();
+
+        roadBike1.setBrand("Specialized");
+        roadBike1.setTireWidth(25);
+        roadBike1.speedAcceleration(15);
+        roadBike1.gearChanges(4);
+        roadBike1.printInfo();
     }
 }
     
