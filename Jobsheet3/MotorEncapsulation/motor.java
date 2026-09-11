3a888af54f5886e8966e3fdc460e282cdc917c4a
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2026-09-11 08:56:13 +0700

    Percobaan 2

--- Jobsheet3/MotorEncapsulation/motor.java
+++ Jobsheet3/MotorEncapsulation/motor.java
@@ -1,8 +1,32 @@
 package Jobsheet3.MotorEncapsulation;
 
 public class motor {
-    public int kecepatan = 0;
-    public boolean kontakOn = false;
+    private  int kecepatan = 0;
+    private  boolean kontakOn = false;
+
+    public void nyalakanMesin(){
+        kontakOn = true;
+    }
+    public void matikanMesin(){
+        kontakOn = false;
+        kecepatan = 0;
+    }
+    public  void tambahKecepatan(){
+        if (kontakOn == true){
+            kecepatan += 5;
+        } 
+        else{
+            System.out.println("Kecepatan tidak bisa bertambah karena Mesin OFF! \n");
+        }
+    }
+    public void kurangiKecepatan(){
+        if (kontakOn == true){
+            kecepatan -= 5;
+        } 
+        else{
+            System.out.println("Kecepatan Tidak Bisa berkurang karena Mesin OFF! \n");
+        }
+    }
 
     public void printStatus(){
         if (kontakOn == true){
