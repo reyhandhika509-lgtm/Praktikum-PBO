1767d3c0e03c06ce87de08b89aa46935425f23e2
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2026-09-11 10:49:33 +0700

    Tugas 1

---
+++ Jobsheet3/tugas/EncapTest.java
@@ -0,0 +1,13 @@
+package Jobsheet3.tugas;
+
+public class EncapTest {
+public static void main(String[] args) {
+    
+    EncapDemo encap = new EncapDemo();
+    encap.setName("james");
+    encap.setAge(35);
+
+    System.out.println("Namee : " + encap.getName());
+    System.out.println("Age : "+ encap.getAge());
+    }
+}
