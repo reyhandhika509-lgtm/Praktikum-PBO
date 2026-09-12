c7ad2f4efbe909ca10e414af3b4f5cb70e07a7f4
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2026-09-12 20:39:11 +0700

    modifikasi

--- Jobsheet3/tugas/TestLogistik.java
+++ Jobsheet3/tugas/TestLogistik.java
@@ -1,28 +1,71 @@
 package Jobsheet3.tugas;
 
+import java.util.Scanner;
 public class TestLogistik {
     public static void main(String[] args) {
 
-        Kontainer kontainerAlfa = new Kontainer("REQ-998", "PT. Maju Bersama", 5000);
+        Scanner input = new Scanner(System.in);
 
-        System.out.println("Nama Pemilik Kontainer: " + kontainerAlfa.getNamaPemilik());
-        System.out.println("Kapasitas Maksimal: " + kontainerAlfa.getKapasitasMaksimal() + " kg");
-    
-        System.out.println("\nMemasukkan muatan baru seberat 6.000 kg...");
-        kontainerAlfa.tambahMuatan(6000);
-        System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
+        
+        Kontainer kontainer1 = new Kontainer(
+            "RESI001",
+            "Reyhandhika",
+            1000
+        );
 
-        System.out.println("\nMemasukkan muatan baru seberat 4.000 kg...");
-        kontainerAlfa.tambahMuatan(4000);
-        System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
+        System.out.println("=== SISTEM LOGISTIK KONTTAINER ===");
 
-        System.out.println("\nMembongkar muat/menurunkan barang seberat 500- kg...");
-        kontainerAlfa.turunkanMuatan(500);
-        System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
+        System.out.println("Nomor Resi: "
+                + kontainer1.getNomorResi());
 
-        System.out.println("\nMembongkar muat/menurunkan barang seberat 1.500 kg...");
-        kontainerAlfa.turunkanMuatan(1500);
-        System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
+        System.out.println("Nama Pemilik: "
+                + kontainer1.getNamaPemilik());
+
+        System.out.println("Kapasitas Maksimal: "
+                + kontainer1.getKapasitasMaksimal() + " kg");
+
+
+        System.out.print("\nMasukkan berat muatan: ");
+        double tambah = input.nextDouble();
+
+        kontainer1.tambahMuatan(tambah);
+
+        System.out.println("Muatan saat ini: "
+                + kontainer1.getBeratMuatanSaatIni() + " kg");
+
+
+        System.out.print("\nMasukkan berat yang akan diturunkan: ");
+        double turun = input.nextDouble();
+
+        kontainer1.turunkanMuatan(turun);
+
+        System.out.println("Muatan saat ini: "
+                + kontainer1.getBeratMuatanSaatIni() + " kg");
+
+        input.close();
     }
-    
 }
+
+//         Kontainer kontainerAlfa = new Kontainer("REQ-998", "PT. Maju Bersama", 5000);
+
+//         System.out.println("Nama Pemilik Kontainer: " + kontainerAlfa.getNamaPemilik());
+//         System.out.println("Kapasitas Maksimal: " + kontainerAlfa.getKapasitasMaksimal() + " kg");
+    
+//         System.out.println("\nMemasukkan muatan baru seberat 6.000 kg...");
+//         kontainerAlfa.tambahMuatan(6000);
+//         System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
+
+//         System.out.println("\nMemasukkan muatan baru seberat 4.000 kg...");
+//         kontainerAlfa.tambahMuatan(4000);
+//         System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
+
+//         System.out.println("\nMembongkar muat/menurunkan barang seberat 500- kg...");
+//         kontainerAlfa.turunkanMuatan(500);
+//         System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
+
+//         System.out.println("\nMembongkar muat/menurunkan barang seberat 1.500 kg...");
+//         kontainerAlfa.turunkanMuatan(1500);
+//         System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
+//     }
+    
+// }
