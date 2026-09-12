3ac57de6dcbb1a86cea76666ecf94a8a1a2a5b50
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2026-09-12 20:39:47 +0700

    Tugas &

---
+++ Jobsheet3/tugas/Tiket.java
@@ -0,0 +1,36 @@
+package Jobsheet3.tugas;
+
+public class Tiket {
+    private String judulFilm;
+    private double hargaDasar;
+    private boolean statusPembayaran;
+
+    public Tiket(String judulFilm, double hargaDasar) {
+        this.judulFilm = judulFilm;
+        
+        if (hargaDasar < 0) {
+            this.hargaDasar = 35000;
+        } else {
+            this.hargaDasar = hargaDasar;
+        }
+        
+        this.statusPembayaran = false;
+    }
+
+    public String getJudulFilm() {
+        return judulFilm;
+    }
+
+    public double getHargaDasar() {
+        return hargaDasar;
+    }
+
+    public boolean isStatusPembayaran() {
+        return statusPembayaran;
+    }
+
+    public void lakukanPembayaran() {
+        this.statusPembayaran = true;
+    }
+    
+}
