31b3fd71f219ac9f8b441bf0e21b9b0e2b3752ee
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2026-09-15 19:56:50 +0700

    Percobaan 2

---
+++ Jobsheet4/id/ac/polinema/relasiclass/percobaan2/Sopir.java
@@ -0,0 +1,24 @@
+package Jobsheet4.id.ac.polinema.relasiclass.percobaan2;
+
+public class Sopir {
+    private String nama;
+    private int biaya;
+
+    public Sopir(){
+    }
+    public void setNama(String nama){
+        this.nama =nama;
+    }
+    public String getNama(){
+        return nama;
+    }
+    public void setBiaya(int biaya){
+        this.biaya = biaya;
+    }
+    public int getBiaya(){
+        return biaya;
+    }
+    public int hitungBiayaSopir(int hari){
+        return biaya * hari;
+    }
+}
