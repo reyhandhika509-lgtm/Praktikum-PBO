effb6f6f961dd9a3c56c5d4c29ed81e2e5ebd6dd
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2026-09-18 16:50:47 +0700

    Percobaan 5

---
+++ Jobsheet4/percobaan5/MainPercobaan5.java
@@ -0,0 +1,8 @@
+package Jobsheet4.percobaan5;
+
+public class MainPercobaan5 {
+    public static void main(String[] args) {
+        Mobil mobil = new Mobil("Avanza");
+        mobil.tampilkanInfo();
+    }
+}
