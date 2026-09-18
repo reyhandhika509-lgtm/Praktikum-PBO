2555dea34721267fb853c802182741b6d94c8d10
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2026-09-18 10:33:46 +0700

    modifikasi percobaan 3

--- Jobsheet4/id/ac/polinema/relasiclass/percobaan3/KeretaApi.java
+++ Jobsheet4/id/ac/polinema/relasiclass/percobaan3/KeretaApi.java
@@ -34,7 +34,9 @@ public class KeretaApi {
         info += "Nama: " + this.nama + "\n";
         info += "Kelas: " + this.kelas + "\n";
         info += "Masinis: " + this.masinis.info() + "\n";
-        info += "Asisten: " + this.asisten.info() + "\n";
+        if (this.asisten != null) {
+            info += "Asisten: " + this.asisten.info() + "\n";
+        }
         return info;
     }
 }
