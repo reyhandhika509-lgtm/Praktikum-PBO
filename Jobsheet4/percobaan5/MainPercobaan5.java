b185af0c626772a446d3a6c5b49afb10aba6f1b7
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2026-09-18 16:59:01 +0700

    Delete Jobsheet4/percobaan5 directory

--- Jobsheet4/percobaan5/MainPercobaan5.java
+++
@@ -1,8 +0,0 @@
-package Jobsheet4.percobaan5;
-
-public class MainPercobaan5 {
-    public static void main(String[] args) {
-        Mobil mobil = new Mobil("Avanza");
-        mobil.tampilkanInfo();
-    }
-}
